9a05356ccf53d977dc35f29fb6b6bad8e2e9139c
yu-hanseo <samryong21@gmail.com>
2024-01-06 20:33:58 +0900

    postsController 테스트 코드 추가

--- src/test/java/com/example/demo/controller/PostCreateControllerTest.java
+++ src/test/java/com/example/demo/controller/PostCreateControllerTest.java
@@ -1,7 +1,7 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.dto.PostCreateDto;
 import com.example.demo.model.dto.UserCreateDto;
-import com.example.demo.model.dto.UserUpdateDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
@@ -9,57 +9,51 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureTestDatabase
-@SqlGroup(
-        @Sql(value = "/sql/delete-all-data.sql")
-)
-public class UserCreateControllerTest {
+@SqlGroup({
+        @Sql(value = "/sql/post-create-controller-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
+        @Sql(value = "/sql/delete-all-data.sql",executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+})
+public class PostCreateControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-    @MockBean
-    private JavaMailSender javaMailSender;
-
     private ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
-    void 사용자는_회원_가입을_할_수있고_회원가입된_사용자는_PENDING_상태이다() throws Exception {
+    void 사용자는_게시물을_작성할_수_있다() throws Exception {
         //given
-        UserCreateDto userCreateDto = UserCreateDto.builder()
-                .email("samryong21@gmail.com")
-                .nickname("samryong")
-                .address("Songpa")
+        PostCreateDto postCreateDto = PostCreateDto.builder()
+                .writerId(1)
+                .content("helloworld")
                 .build();
-        BDDMockito.doNothing().when(javaMailSender).send(any(SimpleMailMessage.class));
         //when
         //then
         mockMvc.perform(
-                        post("/api/users")
-                                .header ("EMAIL","samryong21@gmail.com")
+                        post("/api/posts")
                                 .contentType(MediaType.APPLICATION_JSON)
-                                .content(objectMapper.writeValueAsString(userCreateDto))
+                                .content(objectMapper.writeValueAsString(postCreateDto))
                 )
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.id").isNumber())
-                .andExpect(jsonPath("$.email").value("samryong21@gmail.com"))
-                .andExpect(jsonPath("$.nickname").value("samryong"))
-                .andExpect(jsonPath("$.status").value("PENDING"));
+                .andExpect(jsonPath("$.content").value("helloworld"))
+                .andExpect(jsonPath("$.writer.id").isNumber())
+                .andExpect(jsonPath("$.writer.nickname").value("samryong"))
+                .andExpect(jsonPath("$.writer.email").value("samryong21@gmail.com"));
 
     }
-
 }
---
+++ src/test/resources/sql/post-controller-data.sql
@@ -0,0 +1,5 @@
+
+insert into `users` (`id`, `address`, `certification_code`, `email`, `last_login_at`, `nickname`, `status`)
+values (1, 'Seoul', 'aaaaaaaa-aaaa-aaaa-aaaaaaaaaaaa','samryong21@gmail.com',0,'samryong','ACTIVE');
+insert into `posts` (`id`,`content`, `created_at`, `modified_at`, `user_id`)
+values (1, 'helloworld', 1678530673958,1678530673958,1);
